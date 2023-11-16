4aa8fd75b89b2278ee465ea524eaaafba456dfd6
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2023-11-16 15:09:10 +0545

    gauguin: Switch to common QTI vibrator AIDL
    Change-Id: I2b0e8e42fcc31d8b14e9b8ec8a4d30a97700db8a

--- device.mk
+++ device.mk
@@ -502,10 +502,10 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.xiaomi_gauguin
+    vendor.qti.hardware.vibrator.service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/vibrator/etc/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:vendor/etc/excluded-input-devices.xml
 
 # WiFi
 PRODUCT_PACKAGES += \
