2852c568b4ae71b976b62b4d410243d4eb38bbb3
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2023-12-11 20:15:57 +0545

    gauguin: Enable Pixel goodies
    Change-Id: I70d1954fe52cd8e0e89df1867965591179cf57da

--- aosp_gauguin.mk
+++ aosp_gauguin.mk
@@ -13,6 +13,10 @@ $(call inherit-product, device/xiaomi/gauguin/device.mk)
 
 # Inherit some common PixelOS stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_gauguin
