6707581058d7bb04c436825b3cf0f675ce591b26
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2024-03-16 11:35:56 +0545

    gauguin: Inherit common Xiaomi FCM
    Change-Id: I59bb80de45dcd72400817e019cae292d00973979

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,8 +88,8 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/configs/vintf/xiaomi_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/aosp/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
