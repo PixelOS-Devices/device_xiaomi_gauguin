31e6777a56eedfe69c8ea46f9b3eb63e08290cfb
Dhina17 <dhinalogu@gmail.com>
2024-05-27 19:19:11 +0530

    gauguin: Implement torch light control
    [RealJohnGalt:] modify for oneplus usage, add toggle switch
    [hridaya]: Adapt for gauguin
    
    Change-Id: Icd32d1f6aedb55462c9df4d7cc63a2a4c4e4263e

--- device.mk
+++ device.mk
@@ -123,6 +123,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 $(call soong_config_set,camera,override_format_from_reserved,true)
+$(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservice_extension.xiaomi_gauguin)
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
