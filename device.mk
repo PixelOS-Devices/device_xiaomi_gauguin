64bc0fbce125568493c5fb398c08deec06fd94ee
Kelvin Zhang <zhangkelvin@google.com>
2024-05-28 09:19:54 -0700

    gauguin: Allow to access bootable/deprecated-ota targets
    gauguin is still using non-AB update.
    non-AB code will be moved to a separated namespace, add
    deprecated-ota to gauguin namespace list ahead of time to avoid
    breakage.
    
    Test: th
    Bug: 324360816
    Change-Id: I909064be36e8febe80492cf010eb6eac3d6bc33d

--- device.mk
+++ device.mk
@@ -39,6 +39,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    bootable/deprecated-ota \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
