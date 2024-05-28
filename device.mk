70d2c7f3bfdd6d96f45c1d921a1f4cfbe1e0faa8
basamaryan <basam.aryan@gmail.com>
2024-05-29 02:11:18 +0530

    gauguin: Switch to the pixel power-libperfmgr
    Change-Id: Ic018a0151f7156eab2bb983e3de52cd7b8756710

--- device.mk
+++ device.mk
@@ -42,7 +42,6 @@ PRODUCT_SOONG_NAMESPACES += \
     bootable/deprecated-ota \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi \
     vendor/qcom/opensource/usb/etc
@@ -313,7 +312,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr \
+    android.hardware.power-service.pixel-libperfmgr \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
