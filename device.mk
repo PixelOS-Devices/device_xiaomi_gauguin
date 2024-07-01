0525d25e9908a3cd73372e5381a56ccb10a97d51
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2024-07-01 20:52:42 +0545

    gauguin: overlays: Migrate to GauguinApertureOverlay RRO
    Change-Id: Ic0971294466ada7a8910addc52433ba2bd8af335

--- device.mk
+++ device.mk
@@ -26,6 +26,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
+    GauguinApertureOverlay \
     GauguinCarrierConfigOverlay \
     GauguinFrameworksOverlay \
     GauguinSettingsOverlay \
