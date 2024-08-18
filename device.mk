73d7c75ab4cb75bca4f271918bc545dbc41e30f0
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-08-18 23:59:08 +0200

    gauguin: Move to Xiaomi IR AIDL
    Change-Id: I46094bcbfaba252b5d98ea8211b927fdaa079e2d

--- device.mk
+++ device.mk
@@ -292,7 +292,7 @@ PRODUCT_PACKAGES += \
 
 # IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir-service.example
+    android.hardware.ir-service.xiaomi
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
