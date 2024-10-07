3b737fce1a6e965a0b770a6dde3d41401377fb78
bengris32 <bengris32@protonmail.ch>
2024-10-07 23:50:11 +0100

    gauguin: camera: Implement supportsSetTorchModeExt
    Change-Id: I2ec2f4a30723763e6123a1b742468752f38e3d2f
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- camera/CameraProviderExtension.cpp
+++ camera/CameraProviderExtension.cpp
@@ -39,6 +39,10 @@ bool supportsTorchStrengthControlExt() {
     return true;
 }
 
+bool supportsSetTorchModeExt() {
+    return false;
+}
+
 int32_t getTorchDefaultStrengthLevelExt() {
     return 7;
 }
